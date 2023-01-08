63875c245902156f985f4478046a0f5d6a667eb3
DeviousLab <deviouslab@gmail.com>
2023-01-08 23:04:37 +0400

    initalised realtime chat app project
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Realtime Chat App</title>
+        <meta name="description" content="An app to chat with people in real time" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h1>Homepage</h1>
+      </main>
+    </>
+  )
+}
