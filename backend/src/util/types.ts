2b35a5e65d5e2c7359d9e554e2d8b9fc2249e724
DeviousLab <deviouslab@gmail.com>
2023-02-08 23:35:23 +0400

    fixed issue with next-auth session type extension
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

--- backend/src/util/types.ts
+++ backend/src/util/types.ts
@@ -1,59 +1,74 @@
-import { Prisma, PrismaClient } from "@prisma/client";
-import { PubSub } from "graphql-subscriptions";
-import { Context } from "graphql-ws/lib/server";
-import { Session } from "next-auth"
+import { Prisma, PrismaClient } from '@prisma/client';
+import { PubSub } from 'graphql-subscriptions';
+import { Context } from 'graphql-ws/lib/server';
+import { DefaultUser } from 'next-auth';
+import { Session } from 'next-auth';
 
-import { ConversationPopulate, ParticipantPopulate } from "../graphql/resolvers/conversation";
-import { MessagePopulate } from "../graphql/resolvers/messages";
+import {
+	ConversationPopulate,
+	ParticipantPopulate,
+} from '../graphql/resolvers/conversation';
+import { MessagePopulate } from '../graphql/resolvers/messages';
+
+
+declare module 'next-auth' {
+  interface Session {
+		user: DefaultUser & {
+			id: string;
+      username: string;
+      image: string;
+		};
+	}
+}
 
 export interface GraphQLContext {
-  session: Session | null;
-  prisma: PrismaClient;
-  pubsub: PubSub
+	session: Session | null;
+	prisma: PrismaClient;
+	pubsub: PubSub;
 }
 
 export interface SubscriptionContext extends Context {
-  connectionParams: {
-    session?: Session
-  }
+	connectionParams: {
+		session?: Session;
+	};
 }
 
 export interface CreateUserResponse {
-  success: boolean;
-  error?: string;
+	success: boolean;
+	error?: string;
 }
 
 export type ConversationPopulated = Prisma.ConversationGetPayload<{
-  include: typeof ConversationPopulate
-}>
+	include: typeof ConversationPopulate;
+}>;
 
 export type ParticipantPopulated = Prisma.ConversationParticipantGetPayload<{
-  include: typeof ParticipantPopulate
-}>
+	include: typeof ParticipantPopulate;
+}>;
 
 export interface ConversationCreatedSubscriptionPayload {
-  conversationCreated: ConversationPopulated
+	conversationCreated: ConversationPopulated;
 }
 
 export interface SendMessageArgs {
-  id: string;
-  conversationId: string;
-  userId: string;
-  content: string;
+	id: string;
+	conversationId: string;
+	userId: string;
+	content: string;
 }
 
 export type MessagePopulated = Prisma.MessageGetPayload<{
-  include: typeof MessagePopulate
-}>
+	include: typeof MessagePopulate;
+}>;
 
 export interface MessageSendSubscriptionPayload {
-  messageSent: Prisma.MessageGetPayload<{}>
+	messageSent: Prisma.MessageGetPayload<{}>;
 }
 
 export interface ConversationUpdatedSubscriptionPayload {
-  conversationUpdated: ConversationPopulated
-  }
+	conversationUpdated: ConversationPopulated;
+}
 
 export interface ConversationDeletedSubscriptionPayload {
-  conversationDeleted: ConversationPopulated
+	conversationDeleted: ConversationPopulated;
 }
